65ab69587db8aecf75b87cb07f318e404aa9eb45
joanratto <joan.ratto0724@jala.university>
2026-04-24 18:03:50 -0300

    Add fizzbuzz endpoint

---
+++ backend/FizzBuzzAPI/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/fizzbuzz", () =>
+{
+    var fizzbuzz =  Enumerable.Range(1, 100).Select(index =>
+        new FizzBuzz
+        (
+            index,
+            index % 3 == 0  && index % 5 == 0 ? "fizzbuzz" : index % 3 == 0 ? "fizz" : index % 5 == 0 ? "buzz" : index.ToString()
+        ))
+        .ToArray();
+    return fizzbuzz;
+})
+.WithName("GetFizzBuzz");
+
+app.Run();
+
+record FizzBuzz(int Numero, string? Resultado){}
